736479c9bae12885b07057dff3355f9f20f2ada2
Rahma Ayman <rahma.ayman2016@gmail.com>
2023-12-27 19:46:20 +0200

    add surface to MoviesApp

--- app/src/main/java/com/example/moviesappcompose/core/presentation/MoviesApp.kt
+++ app/src/main/java/com/example/moviesappcompose/core/presentation/MoviesApp.kt
@@ -1,9 +1,12 @@
 package com.example.moviesappcompose.core.presentation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.moviesappcompose.ui.theme.MoviesAppComposeTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -13,9 +16,13 @@ fun MoviesApp() {
     MoviesAppComposeTheme {
         SetBarColor(color = MaterialTheme.colorScheme.inverseOnSurface)
         // A surface container using the 'background' color from the theme
-
-        val navController = rememberNavController()
-        MoviesNavGraph(navController = navController)
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            val navController = rememberNavController()
+            MoviesNavGraph(navController = navController)
+        }
     }
 }
 
